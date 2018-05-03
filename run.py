43dd070d659caca81db0c89bddb1d70cf9520812
Gilad Maoz <gilad.maoz@gmail.com>
2018-05-03 14:37:26 +0200

    Added support for fetching Vultr VC2. Added sample files for hosts and keys ability to mix services

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from paramiko.ssh_exception import NoValidConnectionsError
 
 
 class Provision:
-    def __init__(self, current_private_key, key_json, host_json, digitalocean_token=False):
+    def __init__(self, current_private_key, key_json, host_json, digitalocean=False, vultr=False):
         self.ssh_client = paramiko.SSHClient()
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh_client.load_system_host_keys()
@@ -21,14 +21,25 @@ class Provision:
         except IOError:
             self.log.warning(
                 "Can't seem to find the file: {}. Please provide a valid json key file location.".format(key_json))
+
+            self.hosts = {}
+
         try:
-            self.hosts = self.__fetch_digitalocean__(digitalocean_token) if digitalocean_token else json.load(
-                open(host_json))
+            self.hosts.update(self.__fetch_from_cloud__(digitalocean, 'digitalocean')) if digitalocean else None
+        except KeyError:
+            self.log.error("The DigitalOcean token you provided is invalid!")
+
+        try:
+            self.hosts.update(self.__fetch_from_cloud__(vultr, 'vultr')) if vultr else None
+        except KeyError:
+            self.log.error("The Vultr token you provided is invalid!")
+
+        try:
+            json.load(open(host_json)) if host_json else None
         except IOError:
             self.log.warning(
                 "Can't seem to find the file: {}. Please provide a valid json hosts file location.".format(host_json))
-        except KeyError:
-            self.log.warning("The DigitalOcean token you provided is invalid!")
+
         try:
             self.current_private_key = current_private_key
         except IOError:
@@ -37,15 +48,24 @@ class Provision:
                     current_private_key))
 
     @staticmethod
-    def __fetch_digitalocean__(token, default_user='root'):
-        import digitalocean
+    def __fetch_from_cloud__(token, platform, default_user='root'):
         response = {}
-        try:
-            manager = digitalocean.Manager(token=token)
-            [response.update({droplet.name: {"user": default_user, "host": droplet.ip_address}}) for droplet in
-             manager.get_all_droplets()]
-        except digitalocean.baseapi.DataReadError:
-            raise KeyError
+
+        if 'digitalocean' in platform.lower():
+            import digitalocean
+            try:
+                [response.update({droplet.name: {"user": default_user, "host": droplet.ip_address}}) for droplet in
+                 digitalocean.Manager(token=token).get_all_droplets()]
+            except digitalocean.baseapi.DataReadError:
+                raise KeyError
+
+        elif 'vultr' in platform.lower():
+            import vultr
+            try:
+                [response.update({vc2['label']: {"user:": default_user, "host": vc2['main_ip']}}) for vc2 in
+                 vultr.Vultr(token).server.list().values()]
+            except vultr.utils.VultrError:
+                raise KeyError
         return response
 
     def connect(self):
@@ -87,15 +107,23 @@ if __name__ == '__main__':
     parser.add_argument('-k', '--keys_file', metavar='Keys file location',
                         help='Set the location for the key json file(default in the same folder)', required=False)
 
-    parser.add_argument('-s', '--hosts_file', metavar='Hosts file location',
+    parser.add_argument('-H', '--hosts_file', metavar='Hosts file location',
                         help='Set the location for the hosts json file(default in the same folder)', required=False)
 
     parser.add_argument('-d', '--digitalocean', metavar='DigitalOcean token',
                         help='Add DigitalOcean token for provision ssh-keys to all DigitalOcean account droplets',
                         required=False)
+
+    parser.add_argument('-V', '--vultr', metavar='Vultr token',
+                        help='Add Vultr token for provision ssh-keys to all Vultr account vc2 instances',
+                        required=False)
+
+    parser.add_argument('-v', '--version', action='version', version='SSH Key provisioner 0.3')
+
     args = vars(parser.parse_args())
 
     Provision(current_private_key=args.get('identity_file'),
               key_json=args.get('keys_file') if args.get('keys_file') else 'keys.json',
               host_json=args.get('hosts_file') if args.get('hosts_file') else 'hosts.json',
-              digitalocean_token=args.get('digitalocean')).connect()
+              digitalocean=args.get('digitalocean'),
+              vultr=args.get('vultr')).connect()
